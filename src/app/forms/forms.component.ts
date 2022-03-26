72b76f6721675c55ee47654cb9e76ac7383f6665
tswallen <tswallen@outlook.com>
2022-03-26 14:52:09 +1100

    2 files modified, 1 added

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forms } from './forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,37 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms.component.css'],
 })
 export class FormsComponent implements OnInit {
-  forms = [
-    {
-      title: 'Prodromal Questionnaire',
-      time: 5,
-      description:
-        'A short and simple multiple-choice questionnaire about the nature of psychosis.',
-      link: 'prodromal',
-      moreInfoLink: '#',
-    },
-    {
-      title: 'QPE',
-      time: 15,
-      description: 'Lorem Ipsum',
-      link: 'qpe',
-      moreInfoLink: '#',
-    },
-    {
-      title: 'PDI',
-      time: 15,
-      description: 'Lorem Ipsum',
-      link: 'pdi',
-      moreInfoLink: '#',
-    },
-    {
-      title: 'CAPS',
-      time: 15,
-      description: 'Lorem Ipsum',
-      link: 'caps',
-      moreInfoLink: '#',
-    },
-  ];
+  forms = forms;
 
   constructor() {}
 
---
+++ src/app/forms/forms.ts
@@ -0,0 +1,31 @@
+export const forms = [
+  {
+    title: 'Prodromal Questionnaire',
+    time: 5,
+    description:
+      'A short and simple multiple-choice questionnaire about the nature of psychosis.',
+    link: 'prodromal',
+    moreInfoLink: '#',
+  },
+  {
+    title: 'QPE',
+    time: 15,
+    description: 'Lorem Ipsum',
+    link: 'qpe',
+    moreInfoLink: '#',
+  },
+  {
+    title: 'PDI',
+    time: 15,
+    description: 'Lorem Ipsum',
+    link: 'pdi',
+    moreInfoLink: '#',
+  },
+  {
+    title: 'CAPS',
+    time: 15,
+    description: 'Lorem Ipsum',
+    link: 'caps',
+    moreInfoLink: '#',
+  },
+];
